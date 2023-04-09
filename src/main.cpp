62af19ea290cf5861c2994c0a0fe02fdebc67fd1
Edabouba01 <Marikoben111@gmail.com>
2023-04-09 20:02:20 -0300

    commit1 Librairie

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+/*Pour Bme*/
+
+#include <AsyncElegantOTA.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+
+
+#include <WiFi.h>
+/*Pour Ota*/
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+
+#include <Wire.h>
+#include <SPI.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
